76b2393c446d8f4861dedb013a84e936be9d4b76
Expertconnectionconsultant <abplaytv5@gmail.com>
2025-11-12 19:53:52 +0530

    Update phone number positioning: center on mobile, left on desktop - Mobile: top-center with left-1/2 transform -translate-x-1/2 - Desktop: maintain left positioning (md:left-16 lg:left-24)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -56,7 +56,7 @@ const Index = () => {
           />
           
           {/* Phone Number Display */}
-          <div className="absolute top-8 left-8 md:left-16 lg:left-24">
+          <div className="absolute top-8 left-1/2 transform -translate-x-1/2 md:left-16 md:transform-none md:translate-x-0 lg:left-24">
             <div className="flex items-center gap-2 text-white">
               <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z" />
